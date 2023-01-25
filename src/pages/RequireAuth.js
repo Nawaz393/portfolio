88bcc8217f76d32210e8890f758de09d62e71d2b
mohdnawaz6393@gmail.com <mohdnawaz6393@gmail.com>
2023-01-25 20:24:52 +0500

    fixing some jwt issues

--- src/pages/RequireAuth.js
+++ src/pages/RequireAuth.js
@@ -1,10 +1,16 @@
 import { useLocation, Navigate, Outlet } from "react-router-dom";
+import { isJwtExpired } from "jwt-check-expiration";
+
 const RequireAuth = () => {
   const location = useLocation();
-  return JSON.parse(localStorage.getItem("user"))?.name ?  
+  if (isJwtExpired(JSON.parse(localStorage.getItem("user"))?.token)) {
+    localStorage.clear();
+    return <Navigate to="/admin" state={{ from: location }} replace />;
+  }
+  return JSON.parse(localStorage.getItem("user"))?.name ? (
     <Outlet />
-  : 
-    <Navigate to="/admin" state={{ from: location }} replace />  
+  ) : (
+    <Navigate to="/admin" state={{ from: location }} replace />
+  );
 };
-
 export default RequireAuth;
